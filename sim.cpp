37c201ee89a6267e41153b1871182f2ad33ae009
Sohrab83 <90763106+Sohrab83@users.noreply.github.com>
2021-10-06 12:29:05 +0330

    Create sim.cpp

---
+++ sim.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+using namespace std ;
+
+   
+    int b = 0 ;  
+int main()
+{
+    int a[5]={1,2,3,4,50};
+    a[-1]=5;
+    cout<<a[-1];
+}
